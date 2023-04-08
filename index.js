f186d8286b0f6265655a81980713729dbfbe2486
Noble Osinachi <nobleosinachi98@gmail.com>
2023-04-08 10:33:02 +0100

    Update index.js
    Co-authored-by: Abdulazeez <abdulazeezsalihu41@gmail.com>
    Co-authored-by: Alireza Zahiri <itsalireza.zr@gmail.com>
    Co-authored-by: Allan Cruz <57270969+Allanbcruz@users.noreply.github.com>
    Co-authored-by: Ashley Davis <ashley@codecapers.com.au>
    Co-authored-by: Bruno Vaulawerneck <brunovaulawerneck@gmail.com>
    Co-authored-by: Daniel Espendiller <daniel@espendiller.net>
    Co-authored-by: Daniel Lopretto <timemachine3030@users.noreply.github.com>
    Co-authored-by: David Oluwasegun <oluwasegundavid7@gmail.com>
    Co-authored-by: David Popoola <davieedpopoola@gmail.com>
    Co-authored-by: Derrickkoko1234 <kokoderrick3@gmail.com>
    Co-authored-by: George Cheng <Gerhut@gmail.com>
    Co-authored-by: Golden Chukwukere <66950981+iamGolden@users.noreply.github.com>
    Co-authored-by: Ibukun <ibukunoladipupo10@gmail.com>
    Co-authored-by: Isaac Bassey <chartobyson2025@gmail.com>
    Co-authored-by: Jared Collier <jared.j.collier@gmail.com>
    Co-authored-by: Jason Kwok <JasonHK@users.noreply.github.com>
    Co-authored-by: Jato Joseph <jaotjoseph4@gmail.com>
    Co-authored-by: Jay <jasonsaayman@gmail.com>
    Co-authored-by: Joan <sylvesterjoannaunyii@gmail.com>
    Co-authored-by: Jonathan Foster <jonathan@jonathanfoster.io>
    Co-authored-by: Joshua-Chris <joshuadgreat04@gmail.com>
    Co-authored-by: Kevin Kirsche <Kev.Kirsche+GitHub@gmail.com>
    Co-authored-by: Nguyễn Xuân Nhân <nguyenxuannhan407@gmail.com>
    Co-authored-by: Nicolas Riesco <enquiries@nicolasriesco.net>
    Co-authored-by: Noble Osinachi 💭 <150407018@live.unilag.edu.ng>
    Co-authored-by: OrahKokos <marko.paroski.ns@gmail.com>
    Co-authored-by: PraiseGeek <praisegeek@gmail.com>
    Co-authored-by: Precious Adedibu <adedibuprecious@gmail.com>
    Co-authored-by: Remco Haszing <remcohaszing@gmail.com>
    Co-authored-by: Rogelio Morrell <molekilla@gmail.com>
    Co-authored-by: Sanowar Sunny <sanowarhossain3843@gmail.com>
    Co-authored-by: Symbitic <alex.shaw.as@gmail.com>
    Co-authored-by: Taemin Shin <cprayer13@gmail.com>
    Co-authored-by: Tim Gates <tim.gates@iress.com>
    Co-authored-by: Uchendu Oluoma <oluomachizaram@gmail.com>
    Co-authored-by: UchenduOluoma <oluoma_u@gmail.com>
    Co-authored-by: Uchenna Ogbu <boluogbu@gmail.com>
    Co-authored-by: WordKnight <wordedknight@gmail.com>
    Co-authored-by: Xianming Zhong <chinesedfan@qq.com>
    Co-authored-by: Youngsup Kim <idebtor@gmail.com>
    Co-authored-by: Zachary Schroeder <z.schro@gmail.com>
    Co-authored-by: iAmGolden <chuksgold29@gmail.com>
    Co-authored-by: josiahking <josiahaccounts@gmail.com>
    Co-authored-by: masaun <masaun@protonmail.com>
    Co-authored-by: mathias-ndasi <mathiasndasi@gmail.com>
    Co-authored-by: sifatfaysal <sifatfaysall@gmail.com>
    Co-authored-by: woo jae <woojae.jang26@gmail.com>
    Co-authored-by: femotizo <epebinuoluwafemi@gmail.com>

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log("index.js")
+const http = require('http');
+let otpGenerator = require('./otpGenerator');
+otpGenerator = otpGenerator.otpGenerator;
+
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/api/otp') {
+        // Set the response headers
+        res.setHeader('Content-Type', 'text/plain');
+        res.setHeader('Access-Control-Allow-Origin', '*');
+
+
+        // Generate a new OTP
+        const otp = otpGenerator.generateOTP();
+
+        // Send the OTP in the response body
+        res.end(otp);
+    } else {
+        // Send a 404 error for all other requests
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Not Found');
+    }
+});
+
+// Start the server and listen on the specified port
+server.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
